f18a2550d1bf5fcbfba0cffa35049b04347a9210
quasar <dizevbije94@gmail.com>
2023-07-14 00:30:00 +0000

    first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,2 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Description: Outputs a predefined string
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
-}
+#!/bin/bash
+gcc -S -masm=intel $CFILE

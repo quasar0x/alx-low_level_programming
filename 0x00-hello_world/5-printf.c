2e4f91ef264f1227eadf3fd12b42e8168013e336
quasar <dizevbije94@gmail.com>
2023-07-14 00:00:37 +0000

    first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ *  main - Entry point
  *
- * Description: Outputs a predefined string
+ *  Description: Outputs a predifined string
  *
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
 }

a3669faeaf31257f9add88f1ea74a0095acf2f21
quasar0x <dizevbije94@gmail.com>
2023-07-19 16:32:08 +0000

    first commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,22 +9,22 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 
-    /* Add a return statement to handle any other cases */
-    return 0;
+	/* Add a return statement to handle any other cases */
+	return 0;
 }

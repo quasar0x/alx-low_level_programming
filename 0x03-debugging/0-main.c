4ee9cb4b0a1aec243cc788e698c6ff219dd95307
quasar0x <dizevbije94@gmail.com>
2023-07-21 11:57:48 +0000

    first commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,24 @@
 #include "main.h"
-/**
-* main - Test function for positive or negative
-* Return: 0
-*/
 
+/**
+ * This is the beginning of the main program.
+ * It includes main.h, which should contain the function prototype for positive_or_negative.
+ */
+
+/**
+ * main - Test function for positive or negative
+ * Return: 0
+ */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i;
+
+    /* Initialize i to 0 */
+    i = 0;
+
+    /* Call positive_or_negative function with i */
+    positive_or_negative(i);
+
+    /* End of main function */
+    return (0);
 }
